84a6890691a2fcdf9abd6a6e65d4edc3ce62763c
jujdu <sidoruk.misha@gmail.com>
2020-01-24 14:02:34 +0300

    refactor notification and vibration

---
+++ Sleepy-time/Services/Workers/AVPlayerWorker.swift
@@ -0,0 +1,9 @@
+//
+//  AVPlayerWorker.swift
+//  Sleepy-time
+//
+//  Created by Michael Sidoruk on 24.01.2020.
+//  Copyright © 2020 Michael Sidoruk. All rights reserved.
+//
+
+import Foundation
---
+++ Sleepy-time/Utils/Notfication.Name + Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Notfication.Name + Extensions.swift
+//  Sleepy-time
+//
+//  Created by Michael Sidoruk on 24.01.2020.
+//  Copyright © 2020 Michael Sidoruk. All rights reserved.
+//
+
+import Foundation
